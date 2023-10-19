2c7c689bad071fdb26504bb793764c528d22e089
Stephanie Goodie-Obi <goodieobistephanie@gmail.com>
2023-10-19 12:56:03 -0400

    initial commit

--- src/PolarisChip.js
+++ src/PolarisChip.js
@@ -3,24 +3,67 @@ import { LitElement, html, css } from 'lit';
 export class PolarisChip extends LitElement {
   static get properties() {
     return {
-      title: { type: String },
+      name: { type: String },
+      link: {type: String},
+      active: { type: Boolean, reflect: true }
     };
   }
 
   static get styles() {
     return css`
       :host {
-        display: block;
+        display: inline-block;
+        margin:0 12px 12px 0;
       }
+      .link:hover,
+      :host([active]) .link { 
+        background-color : #005fa9;
+        border: 2px solid #005fa9;
+        border-radius: 3px;
+        color: #ededed;
+        font-size: 16px;
+        font-weight: bold;
+        cursor: pointer;
+      }
+      .link{ 
+        font-weight: bold; 
+        text-decoration: none; 
+        padding: 8px 4px; 
+        border: 2px solid #3681bb;
+        border-radius: 3px;
+        color: #3681bb;
+        font-size: 16px;
+      }
+      .link:focus{
+        background-color : #45637c;
+        border: 2px solid #45637c;
+        border-radius: 3px;
+        color: #cce9ff;
+        font-size: 16px;
+        font-weight: bold;
+        cursor: pointer;
+      }
+
+      span {
+        padding: 8px 4px;
+        border: 2px solid #3681bb;
+        border-radius: 5px;
+        color: #fff;
+        font-size: 16px;
+      }
+
     `;
   }
 
   constructor() {
     super();
-    this.title = 'My boilerplate';
+    //this.title = 'My boilerplate';
+    this.name = 'Football';
+    this.link = "https://www.psu.edu/news/research";
+  
   }
 
   render() {
-    return html`<span>${this.title}</span>`;
+    return html`<a class="link" href="${this.link}"><slot>${this.name}</slot></a>`;
   }
 }
